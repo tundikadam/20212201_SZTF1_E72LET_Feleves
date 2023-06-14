7a258dee26f37b4faee2df78ddbad0ae82ece685
tundik.adam <adamtundik@stud.uni-obuda.hu>
2023-06-14 18:40:56 +0200

    Add project files.

---
+++ 20212201_SZTF1_E72LET_Feleves/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20212201_SZTF1_E72LET_Feleves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
